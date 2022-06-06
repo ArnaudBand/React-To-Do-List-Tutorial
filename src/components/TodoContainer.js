9659d91d9ebfecab5a70b6021c1edcb5488fa4e2
Arnaud <bandonkeyea@gmail.com>
2022-06-06 10:38:38 -0600

    Create components

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+import InputTodo from './InputTodos';
+
+export const TodoContainer = () => {
+    return (
+        <>
+          <Header />
+          <InputTodo />
+        </>
+    )
+}
