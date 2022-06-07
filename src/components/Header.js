4173686a399ef53c5faca638139c14e0281a016f
Arnaud <bandonkeyea@gmail.com>
2022-06-07 10:24:51 -0600

    Update the styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,11 @@
 import React from 'react';
 
-const Header = () => {
-  const headerStyle = {
-    padding: '10px 0',
-    lineHeight: '1.5em',
-    border: '1px solid #000',
-  };
-
-  return (
-    <header style={headerStyle}>
-      <h1
-        style={{
-          fontSize: '2em',
-          fontWeight: '600',
-          lineHeight: '1em',
-          color: '#eec',
-          textTransform: 'lowercase',
-          textAlign: 'center',
-        }}
-      >
-        todos
-      </h1>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="headerStyle">
+    <h1>
+      todos
+    </h1>
+  </header>
+);
 
 export default Header;
